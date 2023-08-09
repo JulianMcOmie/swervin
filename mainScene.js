0e1ae31d23f8c62888500b4ba133cd6f536c8366
Julian McOmie <jmcomie@uw.edu>
2023-08-08 21:58:50 -0700

    speed now changes based on mouse distance

--- mainScene.js
+++ mainScene.js
@@ -4,13 +4,35 @@ class MainScene extends Phaser.Scene {
     }
 
     create() {
+        // Add a text to toggle fullscreen
+        var fullScreenText = this.add.text(10, 10, 'Go Fullscreen', { fill: '#fff' });
+        fullScreenText.setInteractive();
+        
+        fullScreenText.on('pointerup', function() {
+            if (this.scale.isFullscreen) {
+                this.scale.stopFullscreen();
+            } else {
+                this.scale.startFullscreen();
+            }
+        }, this);
+
+        // Alternatively, you can also use a key press to toggle fullscreen
+        var key = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.F);
+        key.on('down', function() {
+            if (this.scale.isFullscreen) {
+                this.scale.stopFullscreen();
+            } else {
+                this.scale.startFullscreen();
+            }
+        }, this);
+            
         this.graphics = this.add.graphics();
 
         this.graphics.fillStyle(0x0000ff);
         this.graphics.fillCircle(400, 300, 30);
 
         //this.cameras.main.setBackgroundColor("#0000FF");
-        this.matter.world.setBounds(0, 0, 800, 600);
+        this.matter.world.setBounds(0, 0, window.innerWidth, window.innerHeight);
 
         this.circle = this.matter.add.circle(400, 300, 50, {
             friction: 0.005,
@@ -22,22 +44,26 @@ class MainScene extends Phaser.Scene {
     update() {
         this.graphics.clear();
         const pointer = this.input.activePointer;
-        console.log('Pointer:', pointer);
     
         const target = new Phaser.Math.Vector2(pointer.x, pointer.y);
-        console.log('Target:', target);
+        
+        let velocity = target.subtract(new Phaser.Math.Vector2(this.circle.position.x, this.circle.position.y));
+        const maxVelocity = velocity.normalize().scale(150);
+        if (velocity.length() > maxVelocity.length()) {
+            velocity = maxVelocity;
+        }
+
+        let speed = Math.sqrt(velocity.x ** 2 + velocity.y ** 2);
+
+        let forceMagnitude = 0.0019; // Replace with the magnitude of the force you want to apply
+        let force = { x: velocity.x * forceMagnitude, y: velocity.y * forceMagnitude };
     
-        const velocity = target.subtract(new Phaser.Math.Vector2(this.circle.position.x, this.circle.position.y)).normalize().scale(5);
-        //const velocity = new Phaser.Math.Vector2(pointer.x - this.circle.position.x, pointer.y - this.circle.position.y);
-        velocity.normalize().scale(5);
-        console.log('Velocity:', velocity);
-    
-        this.matter.body.setVelocity(this.circle, { x: velocity.x, y: velocity.y });
-    
+        //this.circle.velocity = 5;
+        this.matter.body.applyForce(this.circle, this.circle.position, force);
+
         this.graphics.fillStyle(0x0000ff);
         this.graphics.fillCircle(this.circle.position.x, this.circle.position.y, 30);
     
-        console.log('Circle Position:', this.circle.position);
     }
     
 }
