97db91e07ed5e46c482f8fed83e599fe0d0f2a6a
Julian McOmie <jmcomie@uw.edu>
2023-08-09 01:56:26 -0700

    multiplayer kind of working! it's pretty messed up though...

--- public/mainScene.js
+++ public/mainScene.js
@@ -5,53 +5,50 @@ class MainScene extends Phaser.Scene {
 
     create() {
         this.socket = io();
-        this.players = {}
+        this.players = {};
 
         this.socket.on('connect', () => {
             console.log('Connected to the server');
             console.log('My socket id is: ', this.socket.id);
-            this.players[this.socket.id] = this.matter.add.circle(300, 400, 50, {
-                friction: 0.005,
-                restitution: 0.0,
-                density: 0.04
-            });
+            this.addSelfPlayer(300, 400);
         });
 
-        // // Listen for 'currentPlayers' event
-        // socket.on('currentPlayers', players => {
-        //     // Iterate over all players
-        //     for (const id in players) {
-        //         if (players.hasOwnProperty(id)) {
-        //             // If the player is the current client
-        //             if (id === socket.id) {
-        //                 // Add a new player controlled by the client
-        //                 addSelfPlayer(players[id].x, players[id].y);
-        //             } else {
-        //                 // Add a new player controlled by others
-        //                 addOtherPlayer(players[id].x, players[id].y);
-        //             }
-        //         }
-        //     }
-        // });
+        // Listen for 'currentPlayers' event
+        this.socket.on('currentPlayers', players => {
+            // Iterate over all players
+            for (const id in players) {
+                if (players.hasOwnProperty(id)) {
+                    // If the player is the current client
+                    if (id === this.socket.id) {
+                        // Add a new player controlled by the client
+                        this.addSelfPlayer(players[id].x, players[id].y);
+                    } else {
+                        // Add a new player controlled by others
+                        this.addOtherPlayer(id, players[id].x, players[id].y);
+                    }
+                }
+            }
+        });
 
-        // // When a new player connects
-        // socket.on('newPlayer', playerData => {
-        //     addOtherPlayer(playerData.x, playerData.y);
-        // });
+        // When a new player connects
+        this.socket.on('newPlayer', (playerData) => {
+            this.addOtherPlayer(playerData.x, playerData.y);
+        });
         
-        // // When a player moves
-        // socket.on('playerMoved', playerData => {
-        //     // You'll need to find this player in your game and update their position.
-        //     // This could involve iterating over all players and matching on ID, or storing
-        //     // your players in an object where the keys are the socket IDs, similar to the server.
-        //     movePlayer(playerData.id, playerData.x, playerData.y);
-        // });
+        // When a player moves
+        this.socket.on('playerMoved', (playerData) => {
+            // You'll need to find this player in your game and update their position.
+            // This could involve iterating over all players and matching on ID, or storing
+            // your players in an object where the keys are the socket IDs, similar to the server.
+            //console.log("Player moved to: " + playerData.x + " " + playerData.y);
+            this.movePlayer(playerData.Id, playerData.x, playerData.y);
+        });
         
-        // // When a player disconnects
-        // socket.on('disconnect', playerId => {
-        //     // You'll need to find this player and remove them from your game.
-        //     removePlayer(playerId);
-        // });
+        // When a player disconnects
+        this.socket.on('disconnect', (playerId) => {
+            // You'll need to find this player and remove them from your game.
+            this.removePlayer(playerId);
+        });
 
         var fullScreenText = this.add.text(10, 10, 'Go Fullscreen', { fill: '#fff' });
         fullScreenText.setInteractive();
@@ -86,15 +83,11 @@ class MainScene extends Phaser.Scene {
 
         if (this.socket && this.socket.id) {
             var currentPlayer = this.players[this.socket.id];
-            //if (!this.players[this.socket.id]) return;
             this.graphics.clear();
             const pointer = this.input.activePointer;
         
             const target = new Phaser.Math.Vector2(pointer.x, pointer.y);
             
-            console.log(Object.keys(this.players).length);
-            console.log(currentPlayer);
-            console.log(this.socket.id);
             let velocity = target.subtract(new Phaser.Math.Vector2(currentPlayer.position.x, currentPlayer.position.y));
             const maxVelocity = velocity.normalize().scale(150);
             if (velocity.length() > maxVelocity.length()) {
@@ -105,6 +98,8 @@ class MainScene extends Phaser.Scene {
             let force = { x: velocity.x * forceMagnitude, y: velocity.y * forceMagnitude };
         
             this.matter.body.applyForce(currentPlayer, currentPlayer.position, force);
+            //console.log("moving player to " + currentPlayer.position.x + ", " + currentPlayer.position.y);
+            this.socket.emit("playerMovement", {x: currentPlayer.position.x, y: currentPlayer.position.y});
         }
 
         for (let playerID in this.players) {
@@ -114,23 +109,36 @@ class MainScene extends Phaser.Scene {
         }
         
 
-        //socket.emit("playerMovement", {x: this.circle.position.x, y: this.circle.position.y});
+        
     }
     
     movePlayer(playerId, x, y) {
-        if (players[playerId]) {
-          players[playerId].position.x = x;
-          players[playerId].position.y = y;
+        //console.log(playerId);
+        if (this.players[playerId]) {
+          this.players[playerId].position.x = x;
+          this.players[playerId].position.y = y;
         }
     };
       
-    addOtherPlayer(playerId, x, y) {
-        players[playerId] = this.matter.add.circle(x, y, 50, {
+    addSelfPlayer(x, y) {
+        this.players[this.socket.id] = this.matter.add.circle(x, y, 50, {
             friction: 0.005,
             restitution: 0.0,
             density: 0.04
         });
-    }
+    };
+
+    addOtherPlayer(playerId, x, y) {
+        this.players[playerId] = this.matter.add.circle(x, y, 50, {
+            friction: 0.005,
+            restitution: 0.0,
+            density: 0.04
+        });
+    };
+
+    removePlayer(playerId) {
+        delete this.players[playerId];
+    };
     
 }
 
--- server.js
+++ server.js
@@ -14,37 +14,40 @@ app.get('/', function (req, res) {
     res.sendFile(__dirname + '/index.html');
 })
 
+// io.on('connection', (socket) => {
+
+// });
+
 io.on('connection', (socket) => {
     console.log('A user connected');
 
+    players[socket.id] = {
+        x: 400,
+        y: 300
+    };
+
+    socket.emit('currentPlayers', players);
+    socket.broadcast.emit('newPlayer', players[socket.id]);
+
+    socket.on('playerMovement', (movementData) => {
+        players[socket.id].x = movementData.x;
+        players[socket.id].y = movementData.y;
+        //console.log("hello");
+        socket.broadcast.emit('playerMoved', {Id: socket.id, x: players[socket.id].x, y: players[socket.id].y});
+    });
+
     socket.on('disconnect', () => {
         console.log('A user disconnected');
+        delete players[socket.id];
+        socket.disconnect();
     });
+
+
 });
 
 server.listen(8080, () => {
     console.log('listening on *:8080');
 });
 
-// let players = {};
+let players = {};
 
-// io.on('connection', socket => {
-//   players[socket.id] = {
-//     x: 400,
-//     y: 300
-//   };
-
-//   socket.emit('currentPlayers', players);
-//   socket.broadcast.emit('newPlayer', players[socket.id]);
-
-//   socket.on('disconnect', () => {
-//     delete players[socket.id];
-//     io.emit('disconnect', socket.id);
-//   });
-
-//   socket.on('playerMovement', (movementData) => {
-//     players[socket.id].x = movementData.x;
-//     players[socket.id].y = movementData.y;
-//     socket.broadcast.emit('playerMoved', players[socket.id]);
-//   });
-// });
